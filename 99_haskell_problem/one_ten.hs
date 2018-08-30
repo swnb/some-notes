7f9fa2f6962c7e4817c3c76744e5c689d8379efe
swnb <1786718956@qq.com>
2018-08-30 14:56:59 +0800

    haskell question

--- 99_haskell_problem/one_ten.hs
+++ 99_haskell_problem/one_ten.hs
@@ -15,8 +15,9 @@ elementAt (x:xs) n = elementAt xs (n-1)
 
 --  长度
 myLength :: [a] -> Int
-myLength [] = 0 
-myLength (x:xs)= 1 + myLength xs 
+myLength [] = 0
+myLength x= foldr (\_ acc -> acc + 1) 0 x 
+-- myLength x= foldl (\acc _ -> acc + 1) 0 x 
 
 -- 反向输出数组
 myReverse :: [a] -> [a]
@@ -24,3 +25,12 @@ myReverse []=[]
 myReverse (x:xs) = myReverse xs ++ [x]
 
 -- 判断数组是否镜像
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome x = x == reverse x
+
+-- 展平一个数组
+-- data NestList a = Elem a | List[NestList a]
+-- myFlatten 
+
+myCompress :: [a] -> [a]
+myCompress 
