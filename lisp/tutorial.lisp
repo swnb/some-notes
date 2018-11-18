bfea4a52ab330749146685ba515178698e64a3f5
swnb <1786718956@qq.com>
2018-11-18 23:33:35 +0800

    add (lisp-tutorial) : finish first tutorial codes for common lisp

--- lisp/tutorial.lisp
+++ lisp/tutorial.lisp
@@ -1,1 +1,25 @@
+;; function lisp
 (defun helloWorld (message) (format "%s" message))
+
+;; four-line code
+
+;; example function
+(defun add (num)(+ num 1))
+
+;; call
+(funcall #'add 1)
+
+;; apply
+(apply #'add (list 1))
+
+;; multiple return value
+(defun multipleReturn () (values 1 2 4))
+
+;; pattern match
+(multiple-value-bind (first second third) (multipleReturn) (values first))
+
+;; list
+(defparameter list' (list "1"  2  t))
+
+(nth 2 list')
+
