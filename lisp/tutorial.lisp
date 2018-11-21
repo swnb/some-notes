536f39de825fbe482a04506872dc03995e6c7f9b
swnb <1786718956@qq.com>
2018-11-21 23:06:19 +0800

    lisp def var

--- lisp/tutorial.lisp
+++ lisp/tutorial.lisp
@@ -25,3 +25,28 @@
 (defparameter list' (list "1"  2  t))
 (nth 2 list')
 
+;; recursion
+(defun findallitem (num lst)
+  (if (null lst)
+      nil
+      (if(eql num (car lst))
+	  t
+	  (findallitem num (cdr lst)))))
+
+
+;; var const
+(defparameter *global-var* 123)
+
+(defconstant global-const 21213)
+
+(boundp global-const)
+
+
+(setf x 120)
+
+(setf y (list x))
+
+
+
+
+
