31256e2591550b84a27887dd18a5b1094b5186c1
swnb <1786718956@qq.com>
2019-01-10 11:51:26 +0800

    add (sleep-sort) : js version

---
+++ lan/sort/sleep-sort/javascript.js
@@ -0,0 +1,22 @@
+const timeout = time =>
+	new Promise(resolve => setTimeout(resolve, time)).catch(console.error);
+
+const sort = async arr => {
+	if (!Array.isArray(arr)) return [];
+
+	const result = [];
+	arr.forEach(async ele => {
+		await timeout(ele * 10);
+		result.push(ele);
+	});
+
+	await timeout(Math.max(...arr));
+	return result;
+};
+
+const test = async () => {
+	const result = await sort([2, 3, 1, 4, 5, 56, 61, 1, 2, 4, 5, 6, 21, 2, 12]);
+	console.log(result);
+};
+
+test();
